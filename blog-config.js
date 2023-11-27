ad836607a53e7d2b4e2230d947edb9f15fc2bdb0
kdkdhoho <hkim4410@naver.com>
2023-11-27 18:30:37 +0900

    불필요한 글 삭제, 설정 변경, 프로필 이미지 변경

--- blog-config.js
+++ blog-config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  title: "kdkdhoho",
-  description: "log4dh",
+  title: "log4dh",
+  description: "",
   author: "kdkdhoho",
   siteUrl: "https://kdkdhoho.github.io/",
   links: {
     github: "https://github.com/kdkdhoho",
     blogger: "https://velog.io/@donghokim1998",
+    email: "mailto:hkim4410@naver.com"
   },
   utterances: {
     repo: "kdkdhoho/kdkdhoho.github.io",
