1fab4ce277a936ed8b47634f2478a4ef7fdc1ed2
kdkdhoho <hkim4410@naver.com>
2023-12-06 23:02:33 +0900

    카카오톡 오픈 채팅방 링크 추가

--- blog-config.js
+++ blog-config.js
@@ -6,7 +6,8 @@ module.exports = {
   links: {
     github: "https://github.com/kdkdhoho",
     blogger: "https://velog.io/@donghokim1998",
-    email: "mailto:hkim4410@naver.com"
+    email: "mailto:hkim4410@naver.com",
+    link: "https://open.kakao.com/o/sDdYkMhf"
   },
   utterances: {
     repo: "kdkdhoho/kdkdhoho.github.io",
